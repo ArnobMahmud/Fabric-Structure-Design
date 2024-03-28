bc2909faf75a6f311913edbd53955e8c2a771514
ArnobMahmud <arnob.tech.me@gmail.com>
2024-03-28 08:14:55 +0600

    update

--- src/components/includes/footer.jsx
+++ src/components/includes/footer.jsx
@@ -4,7 +4,17 @@ const Footer = () => {
   return (
     <>
       <div className="footer">
-        <h5>Developed by Arnob Mahmud</h5>
+        <h6>
+          Developed by&nbsp;
+          <a
+            href="http://github.com/ArnobMahmud"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-github" aria-hidden="true" />
+            &nbsp;Arnob Mahmud
+          </a>
+        </h6>
       </div>
     </>
   );
